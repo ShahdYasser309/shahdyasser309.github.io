682f5b1290e8f94025ff246336d62d5a6ec2b282
ShahdYasser309 <yassershahd001@gmail.com>
2025-12-26 21:14:11 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,44 @@
-// Add emojis to phone & email (safe solution)
-const paragraphs = document.querySelectorAll(".section p");
+const sections = document.querySelectorAll(".section");
 
-paragraphs.forEach(p => {
-  if (p.textContent.includes("Phone Number")) {
-    p.innerHTML = "📞 " + p.innerHTML;
-  }
+function revealSections() {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
 
-  if (p.textContent.includes("Email")) {
-    p.innerHTML = "✉️ " + p.innerHTML;
-  }
+    if (top < windowHeight - 120) {
+      section.classList.add("show");
+    }
+  });
+}
 
+window.addEventListener("scroll", revealSections);
+window.addEventListener("load", revealSections);
+
+
+const title = document.getElementById("id1");
+
+title.addEventListener("mouseenter", () => {
+  title.style.transform = "scale(1.15)";
 });
-// Add LinkedIn icon
-const links = document.querySelectorAll('a[href*="linkedin.com"]');
 
-links.forEach(link => {
-  link.innerHTML = "🔗 " + link.innerHTML;
+title.addEventListener("mouseleave", () => {
+  title.style.transform = "scale(1)";
 });
+const subtitle = document.getElementById("paragraph1");
+const text = subtitle.innerText;
+subtitle.innerText = "";
+
+let i = 0;
+function typingEffect() {
+  if (i < text.length) {
+    subtitle.innerText += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, 65);
+  }
+}
+
+typingEffect();
+
 
 
 
